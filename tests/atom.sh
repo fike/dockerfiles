f6aa768a4a6a7ad481570ff4fc15aac18351d137
Fernando Ike <fike@midstorm.org>
2015-10-23 11:34:47 -0200

    Add atom test in circleci.

--- tests/atom.sh
+++ tests/atom.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+build(){
 if [[ -e ~/docker-atom/image.tar ]]  
   then 
     docker load --input ~/docker-atom/image.tar 
@@ -10,3 +11,8 @@ docker build -t fike/atom ~/dockerfiles/atom
 mkdir -p  ~/docker-atom 
 
 docker save --output ~/docker-atom/image.tar fike/atom
+}
+
+run_test(){
+  docker run -it --name=atom fike/atom atom --version
+}
