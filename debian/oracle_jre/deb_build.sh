745c57dcce13e838a279576b1f5447f02837dbe2
Fernando Ike <fike@midstorm.org>
2015-10-27 18:29:10 -0200

    Fixed syntax errors

--- debian/oracle_jre/deb_build.sh
+++ debian/oracle_jre/deb_build.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+# Copyright (c) 2015 Fernando Ike <fike@midstorm.org>.
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# 
+
+# This script allow automation the build Debian package of Oracle JRE,  
+# it depends the Docker Engine to works. The files are in $HOME/java 
+# directory, as tarball files as debian package. 
+# IMPORTANT: You still need to accept license term to finish 
+# the building.
+#
+
+
 CURL=$(which curl)
 CURL_PAR_CHECK="-s -I"
 URL="http://javadl.sun.com/webapps/download/AutoDL?BundleId=111681"
@@ -7,46 +34,43 @@ SED=$(which sed)
 SED_PAR="-n -e"
 DPKG=$(which dpkg)
 DOCKER=$(which docker)
-BUILDDIR=$HOME/java
+BUILDDIR="/home/builder/java"
+JAVA_WORK_DIR="$HOME/java"
 
-version(){
-REMOTE_VERSION=$($CURL $CURL_PAR_CHECK $URL| $SED $SED_PAR 's/.*\([0-9]u[0-9]*\).*/\1/p' 2> /dev/null)
+function version(){
+REMOTE_VERSION=$($CURL $CURL_PAR_CHECK $URL| $SED $SED_PAR 's/.*\(jre\-[0-9]u[0-9].*\-linux\-x64\.tar\.gz*\).*/\1/p' 2> /dev/null)
+NUMBER_VERSION=$(echo $REMOTE_VERSION | $SED $SED_PAR 's/.*\([0-9]u[0-9]*\).*/\1/p')
 LOCAL_VERSION=$($DPKG -l|grep oracle | awk '{ print $3 }')
 }
 
 get_java(){
-  if [[ ! -d $BUILDDIR ]]
+  if [[ ! -d $JAVA_WORK_DIR ]]
   then 
-    mkdir $BUILDDIR
+    mkdir $JAVA_WORK_DIR
   fi
  echo "Downloading new java version."
- $CURL -s -O --location $URL
+ $CURL -L -C - -s $URL -o $JAVA_WORK_DIR/$REMOTE_VERSION 
 }
 
 
-check_version(){
- version 
- if [[ $REMOTE_VERSION != $LOCAL_VERSION ]] 
+function check_version(){
+  version 
+  if [[ $NUMBER_VERSION != $LOCAL_VERSION ]] 
   then
     echo "Java isn't installed as package, downloading new version."
     get_java
- fi
- break
+  else
+    echo "Oracle JRE version installed is the same the upstream site."
+    exit 0
+  fi
 }
 
-build(){ 
+function build(){ 
   check_version 
-  JRE_FILE=$(find $BUILDDIR -maxdepth 1 -name "jre*.tar.gz" -print | sort | tail -n 1)
+  JRE_FILE=$(find $JAVA_WORK_DIR -maxdepth 1 -name "jre*.tar.gz" -printf '%P\n' | sort | tail -n 1)
   echo "Building Oracle JRE package"
-  $DOCKER run -it -v $HOME/java:/home/builder/java fike/debian:ora-jre fakeroot make-jpkg $JRE_FILE  
+  $DOCKER run -it -v $JAVA_WORK_DIR:$BUILDDIR fike/debian:ora-jre fakeroot make-jpkg $JRE_FILE  
 }
-  
-
-if [[ ! -d $HOME/java ]]; 
-then
-  echo "Creating build java package directory." 
-  mkdir $HOME/java 
-fi
   
 
 build
